5fc0c4a016db3f16375bd512025244af3539155f
Israelxdr2 <arodriguezm34@unemi.edu.ec>
2026-05-27 17:50:59 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+<>
+
+<h1>hola mundo</h1>
+</>
+  )
+}
+function App2() {
+  
+  return (
+<>
+
+<h1>Angel Ismael Rodriguez Molina</h1>
+<h1>Angel Israel Rodriguez Molina</h1>
+</>
+  )
+}
+export default App2
